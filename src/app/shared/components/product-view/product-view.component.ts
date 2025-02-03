1364efc75881f2e5185fbb1e918a1a591efa3b77
davidgm26 <davidgama260402@gmail.com>
2025-02-03 13:55:01 +0100

    Finalizada visualmente vista de producto y catalogo

--- src/app/shared/components/product-view/product-view.component.ts
+++ src/app/shared/components/product-view/product-view.component.ts
@@ -1,46 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavbarComponent } from "../navbar/navbar.component";
-import { ActivatedRoute } from '@angular/router';
-import { ProductoService } from '../../services/producto.service';
-import { Producto } from '../../models/producto';
+  import { Component, OnInit } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { NavbarComponent } from "../navbar/navbar.component";
+  import { ActivatedRoute } from '@angular/router';
+  import { ProductoService } from '../../services/producto.service';
+  import { Producto } from '../../models/producto';
+  import { CarruselComponent } from '../carrusel/carrusel.component';
 
-@Component({
-  selector: 'app-product-view',
-  standalone: true,
-  imports: [CommonModule, NavbarComponent],
-  templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.css']
-})
-export class ProductViewComponent implements OnInit {
+  @Component({
+    selector: 'app-product-view',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent,CarruselComponent],
+    templateUrl: './product-view.component.html',
+    styleUrls: ['./product-view.component.css']
+  })
+  export class ProductViewComponent implements OnInit {
 
-  productId!: string;
+    productId!: string;
 
-  producto!: Producto;
-  constructor(
-    private route: ActivatedRoute,
-    private productoService: ProductoService,
-  ) { }
+    producto!: Producto;
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.productId = params.get('id')!;
-      this.getInformacionProducto(this.productId);
-    }
-    );
-  }
+    productosRelacionados!: Producto[];
+    constructor(
+      private route: ActivatedRoute,
+      private productoService: ProductoService,
+    ) { }
 
-  getInformacionProducto(id:string) {
-    this.productoService.getInformacionProducto(id).subscribe(
-      (res) => {
-        this.producto = res;
-        console.log(this.producto);
-      },
-      (err) => {
-        console.log(err);
+    ngOnInit(): void {
+      this.route.paramMap.subscribe(params => {
+        window.scrollTo(0, 0); 
+        this.productId = params.get('id')!;
+        this.getInformacionProducto(this.productId);
+        this.cargarProductos();
       }
-    );
+      );
+    }
+
+    getInformacionProducto(id:string) {
+      this.productoService.getInformacionProducto(id).subscribe(
+        (res) => {
+          this.producto = res;
+          console.log(this.producto);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+
+    cargarProductos(){
+      this.productoService.getProductosLimitados().subscribe(
+        res=>{
+          this.productosRelacionados = res;
+        },
+        error =>{
+          console.error("Eror al realizar la carga de productos");
+          
+        }
+      )
+      }
+
+
   }
-
-
-}
