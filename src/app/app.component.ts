1b38102388e540f149fb178984b893b0892b7557
davidgm26 <davidgama260402@gmail.com>
2025-02-11 19:06:01 +0100

    Correcion agregar a carrito para no clientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Toast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone: true
+  standalone: true,
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'inciensosDeSevillaFront';
--- src/app/shared/components/tarjeta-producto/tarjeta-producto.component.ts
+++ src/app/shared/components/tarjeta-producto/tarjeta-producto.component.ts
@@ -3,19 +3,23 @@ import { CommonModule } from '@angular/common';
 import { Producto } from '../../models/producto';
 import { RouterLink } from '@angular/router';
 import { CarritoService } from '../../services/carrito.service';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-tarjeta-producto',
   standalone: true,
   imports: [CommonModule,RouterLink],
   templateUrl: './tarjeta-producto.component.html',
-  styleUrls: ['./tarjeta-producto.component.css']
+  styleUrls: ['./tarjeta-producto.component.css'],
+  providers: []
 })
 export class TarjetaProductoComponent {
 
 
   constructor(
     private carritoService: CarritoService,
+    private messageService: MessageService
   ){
     
   }
@@ -25,13 +29,19 @@ export class TarjetaProductoComponent {
   valoracion = 'valoración';
   valoraciones = 'valoraciones';
 
+  
 
   cargarDefault():string {
     return "https://inciensosdesevilla.es/175-large_default/incienso-aroma-de-la-cava.jpg";
   }
 
   agregarCarrito(producto: Producto) {
-    this.carritoService.agregarProductoACarrito(producto);
+    if(localStorage.getItem('token') == null) {
+      this.messageService.add({severity:'error', summary:'Error', detail:'Debes iniciar sesión para agregar productos al carrito', life: 2000});
+    }else{
+      this.carritoService.agregarProductoACarrito(producto);
+      this.messageService.add({severity:'success', summary:'Producto añadido', detail:'Producto añadido al carrito',life: 2000});
+    }
   }
 
 
